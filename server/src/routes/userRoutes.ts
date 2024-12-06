1d1b99e915cda6ab6695f0470dc48a302559ca0c
bhansaliMadhav <bhansalimadhav03@gmail.com>
2024-12-06 17:18:10 +0530

    createed user route to be triiger post trigger

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/userController";
+import { getUsers, postUser } from "../controllers/userController";
 
 const router = Router();
 // route to get list of all the tasks related to the project
 router.get("/", getUsers);
+router.post("/", postUser);
 
 export default router;
